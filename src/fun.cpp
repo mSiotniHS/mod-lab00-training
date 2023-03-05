1ecd458aea1f27abe92630d1bd066e650e178651
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-05 22:16:48 +0300

    Исправил форматирование

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,15 +1,12 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-using namespace std;
-
 int64_t power(int64_t x, uint16_t n) {
-    int64_t powered = 1;
+	int64_t powered = 1;
 
-    for (size_t i = 0; i < n; i++)
-    {
-        powered *= x;
-    }
+	for (std::size_t i = 0; i < n; i++) {
+		powered *= x;
+	}
 
-    return powered;
+	return powered;
 }
