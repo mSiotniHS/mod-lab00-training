e662583a7a0f833c9cb4c753319564613ad302bb
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-05 22:18:57 +0300

    Исправил отступы: теперь пробелы (💀)

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,11 +2,11 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
-	int64_t powered = 1;
+    int64_t powered = 1;
 
-	for (std::size_t i = 0; i < n; i++) {
-		powered *= x;
-	}
+    for (std::size_t i = 0; i < n; i++) {
+        powered *= x;
+    }
 
-	return powered;
+    return powered;
 }
